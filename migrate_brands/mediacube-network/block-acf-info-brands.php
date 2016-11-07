a5ff12296e555ac687527296caeca35b7e4a1165
Your Name <mik.german@gmail.com>
2016-11-07 17:10:39 +0000

    Fixed persons in responsive. Started fixing text blocks

---
+++ migrate_brands/mediacube-network/block-acf-info-brands.php
@@ -0,0 +1,244 @@
+
+    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/assets/font-awesome/css/font-awesome.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/assets/css/brands.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/assets/slick/slick.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/assets/slick/slick-my.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/assets/css/odometer-theme-my.css">
+
+<div id="skrollr-body">   
+    
+    <div class="red_blocks">
+        <div class="center_content">
+            <p id="why">Почему Youtube?</p> 
+            <div class="img_text">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/youtube_brands.png" alt="youtube_logo">
+                <div class="two_texts">
+                    <h2>крупнейшая в мире видеоплатформа</h2>
+                    <p>Ежеминутно на YouTube загружается 400 часов видео!</p>
+                </div>
+            </div>
+        </div>
+        
+        <div class="red_rect one"></div>
+        <div class="red_rect two"></div>
+        <div class="red_rect three"></div>
+        <div class="red_rect four"></div>
+        <div class="red_rect five"></div>
+        <div class="red_rect six"></div>
+        <div class="red_rect seven"></div>
+        <div class="red_rect eight"></div>
+        <div class="red_rect nine"></div>
+        <div class="red_rect ten"></div>
+        <div class="red_rect eleven"></div>
+        <div class="red_rect twelve"></div>
+        <div class="red_rect thirteen"></div>
+        <div class="red_rect fourteen"></div>
+        <div class="red_rect fifteen"></div>
+        <div class="red_rect sixteen"></div>
+    </div>
+
+     
+    <div class="search" data-bottom-top="background-position: -200px -100px;" data-top-bottom="background-position: -200px -500px;">
+        <div class="content_search">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/youtube_brands.png" alt="youtube logo">
+            <div class="bar">
+                <div class="search_bar">
+                    <div class="fill">
+                        <div class="fill_text"></div>
+                    </div>
+                    <div class="button">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/lens.png" alt="lens">
+                    </div>
+                </div>
+                <p>По количеству ежедневных поисковых запросов</p>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="three wrap">
+        <div class="three_sect">
+            <div class="sect one">
+                <div class="content_sect">
+                    <h2 class="odometer" id="sixfour"></h2><h2 class="percent">%</h2>
+                    <p>Интернет пользователей посещают Youtube раз в месяц</p>
+                </div>
+            </div>
+            <div class="sect two">
+                <div class="content_sect">
+                    <h2 class="odometer" id="twoseven"></h2><h2 class="percent">%</h2>
+                    <p>Интернет пользователей заходят на Youtube ежедневно</p>
+                </div>
+            </div>
+            <div class="sect three">
+                <div class="content_sect">
+                    <h2 class="odometer" id="fournine"></h2><h2 class="percent">%</h2>
+                    <p> пользователей Youtube подписаны хотя бы на один канал</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    
+    <div class="users_sect">
+        <p class="who">Кто они, пользователи Youtube?</p>
+      
+        <i class="fa fa-chevron-circle-right" id="next_slick" aria-hidden="true"></i>
+        <i class="fa fa-chevron-circle-left" id="prev_slick" aria-hidden="true"></i>
+
+        <div class="users">
+    
+            <div class="slide" id="gender">
+                <div class="stat_block" id="men">
+                    <p>Мужчины</p>
+                    <h2>47%</h2>
+                </div>
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/gender.png" alt="gender">
+                <div class="stat_block">
+                    <p>Женщины</p>
+                    <h2>53%</h2>
+                </div>
+            </div>
+            <div class="slide" id="age">
+                <div class="person" id="teen">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/teen_usr.png" alt="teen">
+                    <h3>18-24</h3>
+                    <h2>22%</h2>
+                </div>
+                <div class="person" id="twenty">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/twenty_usr.png" alt="twenty">
+                    <h3>25-34</h3>
+                    <h2>35%</h2>
+                </div>
+                <div class="person" id="thirty">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/thirty_usr.png" alt="thirty">
+                    <h3>35-44</h3>
+                    <h2>20%</h2>
+                </div>
+                <div class="person" id="fourty">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/fourty_usr.png" alt="fourty">
+                    <h3>45-54</h3>
+                    <h2>14%</h2>
+                </div>
+                <div class="person" id="fifty">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/fifty_usr.png" alt="fifty">
+                    <h3>55+</h3>
+                    <h2>8%</h2>
+                </div>
+            </div>
+            <div class="slide" id="educ">
+                <div class="person" id="low">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/low.png" alt="low">
+                    <h3>Низкий</h3>
+                    <h2>1%</h2>
+                </div>
+                <div class="person" id="twenty">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/middle.png" alt="middle">
+                    <h3>Средний</h3>
+                    <h2>57%</h2>
+                </div>
+                <div class="person" id="thirty">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/clever.png" alt="clever">
+                    <h3>Высокий</h3>
+                    <h2>42%</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="interest">
+        <div class="interest_container">
+            <p class="what">Чем интересуются Youtube пользователи?</p>
+            <div class="graphs">
+                <div class="graph one">
+                   <h2 class="val">46%</h2>
+                    <div class="fig">
+                        <div class="main_part"></div>
+                        <div class="shadow"></div>
+                    </div>
+                    <p class="descr">Музыка</p> 
+                </div>
+
+                <div class="graph two">
+                    <h2 class="val">34%</h2>
+                    <div class="fig">
+                        <div class="main_part"></div>
+                        <div class="shadow"></div>
+                    </div>
+                    <p class="descr">Юмор</p>
+                </div>
+                <div class="graph three">
+                    <h2 class="val">32%</h2>
+                    <div class="fig">
+                        <div class="main_part"></div>
+                        <div class="shadow"></div>
+                    </div>
+                    <p class="descr">Сделай сам</p>
+                </div>
+                <div class="graph four">
+                    <h2 class="val">22%</h2>
+                    <div class="fig">
+                        <div class="main_part"></div>
+                        <div class="shadow"></div>
+                    </div>
+                    <p class="descr">Еда и рецепты</p>
+                </div>
+                <div class="graph five">
+                    <h2 class="val">19%</h2>
+                    <div class="fig">
+                        <div class="main_part"></div>
+                        <div class="shadow"></div>
+                    </div>
+                    <p class="descr">Мода и красота</p>
+                </div>
+                <div class="graph six">
+                    <h2 class="val">19%</h2>
+                    <div class="fig">
+                        <div class="main_part"></div>
+                        <div class="shadow"></div>
+                    </div>
+                    <p class="descr">Гаджеты и электроника</p>
+                </div>
+                <div class="graph seven">
+                    <h2 class="val">19%</h2>
+                    <div class="fig">
+                        <div class="main_part"></div>
+                        <div class="shadow"></div>
+                    </div>
+                    <p class="descr">Путешествия</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+    <div class="decision">
+        <h3 class="bg_nums">49</h3>
+        <h3 class="main_text"><span class="bold large">49%</span> пользователей находили на <span class="bold">YouTube</span> информацию, которая помогала им принять решение о покупке</h3>
+    </div>
+
+    <div class="offers">
+        <p class="what">Чем интересуются Youtube пользователи?</p>
+        <div class="blocks">
+            <div class="block">
+                <img id="ads" src="<?php echo get_template_directory_uri(); ?>/assets/img/advert.png" alt="ad_rekl">
+                <div class="text">Формат <span class="bold">Video Discovery</span> поможет повысить узнаваемость вашего бренда среди аудитории, которую вы не можете охватить за счет других рекламных проявлений. При этом его стоимость очень низка в сравнении с рекламой на ТВ. </div>
+
+                <div class="text">Благодаря формату рекламы <span class="bold">TrueView in-Stream</span>, вы поможете вашей целевой аудитории узнать о вашей компании или о ваших услугах. True View – это формат рекламы, при котором вы платите только за заинтересованные просмотры. </div>
+            </div>
+            <div class="block">
+                <img id="infl" src="<?php echo get_template_directory_uri(); ?>/assets/img/influencer.png" alt="influencer">
+                <div class="text"><span class="bold">Лидеры мнений</span> - никому так не доверяют в интернете как своим YouTube-любимчикам. И это действительно работает. Упоминание продукта в роликах YouTube-блогеров с правильной подачей – это залог успешной конверсии такого рекламного формата.</div>
+                
+            </div>
+            <div class="block">
+                <img id="tv" src="<?php echo get_template_directory_uri(); ?>/assets/img/tv.png" alt="tv">
+                <div class="text"><span class="bold">Канал Бренда</span> - это возможность сформировать действительно лояльную аудиторию вокруг вашего бренда. </div>
+                <div class="text"><span class="bold">Продюссирвание и производство</span> – никакая другая компания в Беларуси не предлагает ведение YouTube-канала «под ключ»: продюсирование, производство контента, публикация, оптимизация.</div>
+            </div>
+        </div>
+    </div>
+</div> <!-- closing skrollr body -->
+    
